6a5ce35520ed465eaca3b34e86e0728c1ebf6be0
Atharva <mondkar.atharva1995@gmail.com>
2026-03-02 12:08:26 +0530

    appid added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app= Flask(__name__)
 
 @app.route("/")
 def index():
-	return "<h1>Atharva Mondkar</h1>"
+	return "<h1>Atharva Mondkar</h1><br><h1>AppID = 2408934</h1>"
 
 if __name__== "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)

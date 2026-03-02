85f8d50a60de32e3ca36fd7b4404e2f73aa73e8e
Atharva <mondkar.atharva1995@gmail.com>
2026-03-02 12:41:45 +0530

    resume added

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "<h1>Atharva Mondkar</h1><br><h1>AppID = 2408934</h1>"
+    return "<h1>Atharva Mondkar</h1><br><h1>AppID = 10437</h1>"
 
-if __name__== "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
